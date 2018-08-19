b989d84f5e8e4bc57218a8de5d858b9521edc353
jnutmegger@gmail.com <jnutmegger@gmail.com>
2018-08-19 15:20:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,16 @@ function destructivelyAppendKitten(name) {
   kitten.push(name);
   return kitten;
 }
+
+
+  1) Arrays destructivelyAppendKitten(name) appends a kitten to the end of the kittens array:
+
+      Error: Expected [ 'Milo', 'Otis', 'Garfield' ] to equal [ 'Milo', 'Otis','Garfield', 'Ralph' ]
+      + expected - actual
+
+       [
+         "Milo"
+         "Otis"
+         "Garfield"
+      +  "Ralph"
+       ]
