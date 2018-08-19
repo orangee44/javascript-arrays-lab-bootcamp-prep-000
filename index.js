687e6316320bd3ff6cd15af7c0dd98936b0b61c3
jnutmegger@gmail.com <jnutmegger@gmail.com>
2018-08-19 15:35:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function destructivelyRemoveFirstKitten() {
 }
 
 function appendKitten(name) {
-  moreKittens = [...kittens, name];
+  var moreKittens = [...kittens, name];
   return moreKittens;
 }
 
