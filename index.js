ce57084cce6da0952e7c8208002ab9b035e45578
jnutmegger@gmail.com <jnutmegger@gmail.com>
2018-08-19 15:37:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,7 +24,8 @@ function appendKitten(name) {
 }
 
 function prependKitten(name) {
-  var moreKittens = []
+  var moreKittens = [name, ...kittens];
+  return moreKittens;
 }
 
   1) Arrays prependKitten(name) prepends a kitten to the kittens array and returns a new array, leaving the kittens array unchanged:
